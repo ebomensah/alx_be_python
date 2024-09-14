78558c1e192e998569c5d1c5723a98176d42d87d
Ebomensah <ernestmensah02@gmail.com>
2024-09-14 22:14:11 +0000

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,11 +1,11 @@
-#Given the following values to the variables number 1 and number 2: "number 1=5" and "number 2= 20"
-number1 =5
-number2 = 20
+#Given the following values to the variables number 1 and number 2: ["number1 = 10" and "number2 = 5"]
+number1 =10
+number2 = 5
 addition = number1 + number2
-print("addition of 5 and 20 is", addition)
+print("addition of 10 and 5 is", addition)
 subtraction= number2-number1
-print("subtraction of 20 and 5 is", subtraction)
+print("subtraction of 10 and 5 is", subtraction)
 multiplication= number1*number2
-print("multipllication of 5 and 20 is", multiplication)
+print("multiplication of 10 and 5 is", multiplication)
 division= number1/number2
-print("division of 5 by 20 is", division)
+print("division of 10 by 5 is", division)
