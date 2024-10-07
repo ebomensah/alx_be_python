8987dac4affecb62861b0ef6faf53c6fcc9cbf7f
Ebomensah <ernestmensah02@gmail.com>
2024-10-07 17:27:03 +0000

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -11,7 +11,7 @@ class BankAccount: #Create account
             self.account_balance -= amount
             return True
         elif amount > self.account_balance:
-            print("Insufficient funds.")
+            return("Insufficient funds.")
             return False                
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:,.2f}")
