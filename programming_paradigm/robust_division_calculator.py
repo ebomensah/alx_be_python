2d21cbb662fcbe166b62749dd38957bcbc94c940
Ebo Mensah <ernestmensah02@gmail.com>
2024-10-08 00:14:43 +0000

    Test run

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,10 +1,15 @@
-def safe_divide(numerator,denominator):
+def safe_divide(numerator, denominator):
+
     try:
+        numerator = float(numerator)
+        denominator = float(denominator)
         result = numerator / denominator
     except ZeroDivisionError:
         print ("Eror:Cannot divide by zero.")
     except TypeError:
         print ("Error: Please enter numberic values only.")
     else: 
-        return (f"The result of division is: {result}")
+        print (f"The result of division is: {result}")
     
+
+safe_divide(4,6)
---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,19 @@
+import unittest
+
+from simple_calculator import SimpleCalculator
+class TestSimpleCalculor (unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(1, 3), 4)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 2), 3)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 10), 30)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(6, 0), None)                        
